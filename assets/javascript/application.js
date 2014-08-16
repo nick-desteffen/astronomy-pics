895d131193bc99b7d8dac69af08291f7984dea03
Nick DeSteffen <nick.desteffen@gmail.com>
2014-08-16 10:34:12 -0500

    Better apod link matching

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -133,11 +133,11 @@ Apod.View = Backbone.View.extend({
     links.each(function(index, link){
       var href = $(link).attr("href");
       if (href != undefined && !href.match(/http/)) {
-        var date = href.match(/\d+/g)
-        if ((date == undefined) || (moment(date[0], _this.dateFormat).diff(moment(_this.firstApod), 'days') < 1)) {
+        var date = /ap(\d+).html/g.exec(href)
+        if ((date == undefined) || (moment(_(date).last(), _this.dateFormat).diff(moment(_this.firstApod), 'days') < 1)) {
           $(link).attr("href", "http://apod.nasa.gov/apod/" + href);
         } else {
-          $(link).attr("href", date[0]);
+          $(link).attr("href", _(date).last());
           $(link).addClass("exp-link");
         }
       }
